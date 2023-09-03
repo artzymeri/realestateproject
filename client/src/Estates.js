921f4d035e51679c1481dfd55d72fb98567c9716
ArtZymeri <132089036+ArtZymeri@users.noreply.github.com>
2023-09-03 20:45:53 +0200

    Update

--- client/src/Estates.js
+++ client/src/Estates.js
@@ -1,8 +1,30 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import './Estates.css'
+import './Estates.css';
+import EstatesTestTable from "./EstatesTestTable";
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Estates = () => {
+
+    const navigate = useNavigate();
+
+    const estatesTable = EstatesTestTable;
+
+    const [filterActive, setFilterActive] = useState(false);
+
+    const activateFilter = () => {
+        if(filterActive) {
+            setFilterActive(false)
+        } else {
+            setFilterActive(true);
+        }
+    };
+
+    const toHomePage = () => {
+        return (
+            navigate('/')
+        )
+    }
 
     const [data, setData] = useState([])
     useEffect(()=>{
@@ -14,9 +36,25 @@ const Estates = () => {
     ,[])
 
     return (
-        <div className="estates-page-body">
-            
-        </div>
+        <>
+            <div className="estates-navbar">
+                <div className="estates-logo-container" ><img src="logo.png" onClick={toHomePage} /><h1 onClick={toHomePage} >Heimer Real Estate</h1></div>
+                <i class="ri-equalizer-fill" onClick={activateFilter}></i>
+            </div>
+            {filterActive ? <div className="estates-navbar-bottom">aa</div> : null}
+            <div className="estates-container">
+                {data.map((estate)=>{
+                    return (
+                        <div className="estate">
+                            <h1>{estate.title}</h1>
+                            <h1>{estate.meter}</h1>
+                            <h1>{estate.location}</h1>
+                            <h1>{estate.posting}</h1>
+                        </div>
+                    )
+                })}
+            </div>
+        </>
     );
 }
 
---
+++ client/src/EstatesTestTable.js
@@ -0,0 +1,24 @@
+const EstatesTestTable = [
+    {
+        title: 'banese',
+        meter: 35,
+        location: 'prishtin',
+    },
+    {
+        title: 'shpi',
+        meter: 25,
+        location: 'kline',
+    },
+    {
+        title: 'hotel',
+        meter: 115,
+        location: 'vushtrri',
+    },
+    {
+        title: 'pllac',
+        meter: 225,
+        location: 'arllat',
+    },
+];
+
+export default EstatesTestTable;
